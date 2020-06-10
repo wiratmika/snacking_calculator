ec4562675bba9ce883ef65888557f1950befad6c
Wiratmika <wiratmika@gmail.com>
2020-06-10 16:58:02 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:snacking_calculator/pages/participants.dart';
+
+void main() => runApp(SnackingCalculator());
+
+class SnackingCalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Kalkulator Jajan',
+      home: Participants(),
+    );
+  }
+}
---
+++ lib/pages/participants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Participants extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(middle: Text('Jajan Calculator')),
+        child: Center(
+          child: Text('Ulala'),
+        ));
+  }
+}
